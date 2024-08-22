92c67f0f8e3395bb55d7018fced3f47dee94a049
Frsxk <fazaalbanna0410@gmail.com>
2024-08-22 16:58:19 +0700

    Create index.py

---
+++ index.py
@@ -0,0 +1,17 @@
+
+# numbers = [2, 4, 6, 8, 10, 12, 15, 18, 20]
+
+# for number in numbers:
+#     print(number)
+#     if (number % 2 == 1):
+#         break
+
+# for i in range(5):
+#     for j in range(i):
+#         print('*', end='')
+#     print('*')
+
+for i in range(2, 4):
+    for j in range(1, 11):
+        print(i, "*", j, "=", i*j)
+    print("")
