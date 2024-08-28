7f7518c9cc55fcd63c7b1429bbb31569cb2c1baa
Frsxk <fazaalbanna0410@gmail.com>
2024-08-28 19:04:57 +0700

    week 2 wip

--- index.py
+++ index.py
@@ -11,7 +11,12 @@
 #         print('*', end='')
 #     print('*')
 
-for i in range(2, 4):
-    for j in range(1, 11):
-        print(i, "*", j, "=", i*j)
-    print("")
+# for i in range(2, 4):
+#     for j in range(1, 11):
+#         print(i, "*", j, "=", i*j)
+#     print("")
+
+# a = 30
+# b = 20
+# a, b = b, a
+# print(a, b)
